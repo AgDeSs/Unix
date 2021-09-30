799d84fddf38881b22671e7ff860efd224bff6ff
AgDeSs <90023858+AgDeSs@users.noreply.github.com>
2021-09-30 14:33:41 +0300

    Add files via upload

---
+++ server2.py
@@ -0,0 +1,21 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9091))
+sock.listen(2)
+
+msg = ''
+
+while msg != "exit":
+    conn, addr = sock.accept()
+    print("Connected: ", addr)
+    msg = ''
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg += data.decode()
+        conn.send(data)
+    print(msg)
+
+conn.close()
